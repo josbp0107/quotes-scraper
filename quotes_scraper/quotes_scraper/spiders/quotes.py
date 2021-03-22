1a2803d011c703197c00fe7cdc4df60d343ac3b2
josebp <josebp0107@gmail.com>
2021-03-22 11:46:36 -0500

    add multiple callbacks

--- quotes_scraper/quotes_scraper/spiders/quotes.py
+++ quotes_scraper/quotes_scraper/spiders/quotes.py
@@ -15,37 +15,36 @@ class QuotesSpyder(scrapy.Spider):
     # custom_settings nos ayuda a guardar el archivo en lugar de usar el flag -o en consola
     custom_settings = {
         'FEED_URI': 'quotes.json', # Nombre del archivo
-        'FEED_FORMAT': 'json' # Formato del archivo
+        'FEED_FORMAT': 'json', # Formato del archivo
+        'LOG_ENCODING': 'utf-8'
     }
+
+    def parse_only_quotes(self, response, **kwargs):
+        if kwargs:
+            quotes = kwargs['quotes']
+        quotes.extend(response.xpath('//span[@class="text" and @itemprop="text"]/text()').getall())
+        
+        next_page_button_link = response.xpath('//ul[@class="pager"]/li[@class="next"]/a/@href').get()
+        if next_page_button_link:
+            yield response.follow(next_page_button_link, callback = self.parse_only_quotes, cb_kwargs= {'quotes':quotes})
+        else:
+            yield {
+                'quotes': quotes
+            }
+
     # Este metodo siempre tiene que estar en spyder con el nombre de parse
     # Analiza la respuesta HTTP que nos envia la peticion a esta pagina y a partir
     # de esta respuesta traer toda la inforamcion que queremos
     def parse(self, response):
-        # print('*' * 10)
-        # print('\n\n')
-        # print(response.status, response.headers)
-        # print('*' * 10)
-        # print('\n\n')
 
         title = response.xpath('//h1/a/text()').get()
-        #print(f'Title: {title}')
-        #print('\n\n')
 
         quotes = response.xpath('//span[@class="text" and @itemprop="text"]/text()').getall()
-        #print('Quotes')
-        #for quote in quotes:
-            #print(f'- {quote}')
-        #print('\n\n')
 
         top_ten_tags = response.xpath('//div[contains(@class, "tags-box")]/span/a/text()').getall()
-        #print('Top ten tags')
-        #for tag in top_ten_tags:
-            #print(f'- {tag}')
-        #print('\n\n')
 
         yield{ # Return parcial de datos. En este caso devuelve un diccionario
             'title': title,
-            'quotes': quotes,
             'top_ten_tags': top_ten_tags
         }
         # Para guardar en un archivo los datos scrapeado es con el siguiente comando:
@@ -55,5 +54,5 @@ class QuotesSpyder(scrapy.Spider):
         # Valida si existen el boton y lo pasea donde guarda el link
         # y vuelve a ejecutar la funcion parse con el callback = self.parse
         if next_page_button_link:
-            yield response.follow(next_page_button_link, callback = self.parse)
+            yield response.follow(next_page_button_link, callback = self.parse_only_quotes, cb_kwargs= {'quotes':quotes})
             # callback -> una funcion que se va a ejecutar luego de hacer la request al link

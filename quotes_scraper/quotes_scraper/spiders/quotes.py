0a56909a624a05a04a40e6c45c1d3df3a6165f52
josebp <josebp0107@gmail.com>
2021-03-22 11:28:22 -0500

    add quotes to others pages

--- quotes_scraper/quotes_scraper/spiders/quotes.py
+++ quotes_scraper/quotes_scraper/spiders/quotes.py
@@ -3,6 +3,7 @@ import scrapy
 # Titulo = //h1/a/text()
 # Citas = //span[@class="text" and @itemprop="text"]/text()
 # Top ten tags = //div[contains(@class, "tags-box")]/span/a/text()).getall()
+# Next page button = response.xpath('//ul[@class="pager"]/li[@class="next"]/a/@href').get()
 
     # Es importante siempre crear la clase que hereda de scrapy
     # Estos atributos tienen que ir siempre en los archivos de spiders
@@ -11,7 +12,11 @@ class QuotesSpyder(scrapy.Spider):
     start_urls = [  
         'http://quotes.toscrape.com/'
     ] # Esta lista contiene todas las URLS que nos vamos a apuntar, dirigir principalmente, la cual vamos a obtener 
-
+    # custom_settings nos ayuda a guardar el archivo en lugar de usar el flag -o en consola
+    custom_settings = {
+        'FEED_URI': 'quotes.json', # Nombre del archivo
+        'FEED_FORMAT': 'json' # Formato del archivo
+    }
     # Este metodo siempre tiene que estar en spyder con el nombre de parse
     # Analiza la respuesta HTTP que nos envia la peticion a esta pagina y a partir
     # de esta respuesta traer toda la inforamcion que queremos
@@ -45,3 +50,10 @@ class QuotesSpyder(scrapy.Spider):
         }
         # Para guardar en un archivo los datos scrapeado es con el siguiente comando:
         # scrapy crawl quote quote.extension (la extension puede  ser JSON CSV XML ETC)  
+
+        next_page_button_link = response.xpath('//ul[@class="pager"]/li[@class="next"]/a/@href').get()
+        # Valida si existen el boton y lo pasea donde guarda el link
+        # y vuelve a ejecutar la funcion parse con el callback = self.parse
+        if next_page_button_link:
+            yield response.follow(next_page_button_link, callback = self.parse)
+            # callback -> una funcion que se va a ejecutar luego de hacer la request al link

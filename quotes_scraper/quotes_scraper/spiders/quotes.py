3fac75561edce7ef068cc12039b5c0aadb2d3729
josebp <josebp0107@gmail.com>
2021-03-22 10:27:19 -0500

    save scraped data in a file

--- quotes_scraper/quotes_scraper/spiders/quotes.py
+++ quotes_scraper/quotes_scraper/spiders/quotes.py
@@ -16,24 +16,32 @@ class QuotesSpyder(scrapy.Spider):
     # Analiza la respuesta HTTP que nos envia la peticion a esta pagina y a partir
     # de esta respuesta traer toda la inforamcion que queremos
     def parse(self, response):
-        print('*' * 10)
-        print('\n\n')
-        print(response.status, response.headers)
-        print('*' * 10)
-        print('\n\n')
+        # print('*' * 10)
+        # print('\n\n')
+        # print(response.status, response.headers)
+        # print('*' * 10)
+        # print('\n\n')
 
         title = response.xpath('//h1/a/text()').get()
-        print(f'Title: {title}')
-        print('\n\n')
+        #print(f'Title: {title}')
+        #print('\n\n')
 
         quotes = response.xpath('//span[@class="text" and @itemprop="text"]/text()').getall()
-        print('Quotes')
-        for quote in quotes:
-            print(f'- {quote}')
-        print('\n\n')
+        #print('Quotes')
+        #for quote in quotes:
+            #print(f'- {quote}')
+        #print('\n\n')
 
         top_ten_tags = response.xpath('//div[contains(@class, "tags-box")]/span/a/text()').getall()
-        print('Top ten tags')
-        for tag in top_ten_tags:
-            print(f'- {tag}')
-        print('\n\n')
+        #print('Top ten tags')
+        #for tag in top_ten_tags:
+            #print(f'- {tag}')
+        #print('\n\n')
+
+        yield{ # Return parcial de datos. En este caso devuelve un diccionario
+            'title': title,
+            'quotes': quotes,
+            'top_ten_tags': top_ten_tags
+        }
+        # Para guardar en un archivo los datos scrapeado es con el siguiente comando:
+        # scrapy crawl quote quote.extension (la extension puede  ser JSON CSV XML ETC)  
